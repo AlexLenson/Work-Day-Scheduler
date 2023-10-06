703de4c918770e1d8e28ae394e55536de6f44aff
Alex Lenson <144947695+AlexLenson@users.noreply.github.com>
2023-10-06 00:31:43 -0400

    convert to military time

--- script.js
+++ script.js
@@ -78,11 +78,83 @@ $(function () {
 
   // function to update the time by comparing current hour with block hour
   function updateTime() {
-    currentHour = today.format("h")
-    console.log(currentHour);
+
+    var currentHour = today.format("ha");
+
+    // convert currentHour to military time
+    if (currentHour == "12am") {
+      currentHour = 0;
+    } else if (currentHour == "1am") {
+      currentHour = 1;
+    } else if (currentHour == "2am") {
+      currentHour = 2;
+    } else if (currentHour == "3am") {
+      currentHour = 3;
+    } else if (currentHour == "4am") {
+      currentHour = 4;
+    } else if (currentHour == "5am") {
+      currentHour = 5;
+    } else if (currentHour == "6am") {
+      currentHour = 6;
+    } else if (currentHour == "7am") {
+      currentHour = 7;
+    } else if (currentHour == "8am") {
+      currentHour = 8;
+    } else if (currentHour == "9am") {
+      currentHour = 9;
+    } else if (currentHour == "10am") {
+      currentHour = 10;
+    } else if (currentHour == "11am") {
+      currentHour = 11;
+    } else if (currentHour == "12pm") {
+      currentHour = 12;
+    } else if (currentHour == "1pm") {
+      currentHour = 13;
+    } else if (currentHour == "2pm") {
+      currentHour = 14;
+    } else if (currentHour == "3pm") {
+      currentHour = 15;
+    } else if (currentHour == "4pm") {
+      currentHour = 16;
+    } else if (currentHour == "5pm") {
+      currentHour = 17;
+    } else if (currentHour == "6pm") {
+      currentHour = 18;
+    } else if (currentHour == "7pm") {
+      currentHour = 19;
+    } else if (currentHour == "8pm") {
+      currentHour = 20;
+    } else if (currentHour == "9pm") {
+      currentHour = 21;
+    } else if (currentHour == "10pm") {
+      currentHour = 22;
+    } else if (currentHour == "11pm") {
+      currentHour = 23;
+    }
+
+
+    for (let i = 9; i < 18; i++) {
+      var hourID = "#hour-" + i;
+      var blockHour = i;
+      
+      if (blockHour < currentHour) {
+        // past
+      } else if (blockHour == currentHour) {
+        // present
+      } else {
+        // future
+      }
+    }
+    
+    
+
+
   }
 
   updateTime();
+  // const ampm2military = ampm => ampm ? dayjs(`1/1/1 ${ampm}`).format("HH:mm:00") : null;
+
+  // console.log(ampm2military("1:24 PM"));
 
 
   // Event listeners
